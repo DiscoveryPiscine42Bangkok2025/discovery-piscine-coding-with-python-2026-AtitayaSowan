5fa5cbcf8b4961c6b8defd23cb90410f4afcb2b4
AtitayaSowan <66051129@kmitl.ac.th>
2026-02-09 22:18:01 +0700

    Update i_got_that.py

--- cell03/ex02/i_got_that.py
+++ cell03/ex02/i_got_that.py
@@ -1,16 +1,3 @@
-def main():
-    prompt = "What you gotta say? : "
-
-    while True:
-        try:
-            user_input = input(prompt)
-        except (EOFError, KeyboardInterrupt):
-            break
-
-        if user_input == "STOP":
-            break
-        
-        prompt = "I got that! Anything else? : "
-
-if __name__ == "__main__":
-    main()
+x = input(f"What you gotta say? : ")
+while x != "STOP":
+    x = input(f"I got that! Anything else? : ")
