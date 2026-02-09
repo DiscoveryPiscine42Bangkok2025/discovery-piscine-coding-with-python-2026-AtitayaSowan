4f63d08b8dc6790bcd1b573f65780306392c7646
AtitayaSowan <66051129@kmitl.ac.th>
2026-02-09 14:59:12 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,5 @@
+first_name = "Atitaya"
+last_name = "Sowan"
+
+print(first_name)
+print(last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Atitaya"
+last_name = "Sowan"
+whole_name = first_name + " " + last_name
+print(whole_name)
