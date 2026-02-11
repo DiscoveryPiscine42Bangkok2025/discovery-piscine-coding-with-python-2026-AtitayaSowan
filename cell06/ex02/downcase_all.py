3ba9086a11edacca6742981f4c015f5ea544e271
AtitayaSowan <66051129@kmitl.ac.th>
2026-02-11 21:38:34 +0700

    Add files via upload

---
+++ cell06/ex02/downcase_all.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+def downcase_it(string):
+    return string.lower()
+
+args = sys.argv[1:] 
+
+if not args:
+    print("none")
+else:
+    for arg in args:
+        print(downcase_it(arg))
